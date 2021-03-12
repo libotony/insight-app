44b8b8c69033b89f5b325f572c6380dcec2e9fd7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-12 16:56:33 +0000

    Deploy insight.vecha.in to github.com/vechain/insight-app.git:gh-pages

---
+++ precache-manifest.b97e8dffb7ffda97843c69f0b0df81eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bb211ba8b31e5c2d6ba",
+    "url": "css/app.aec32434.css"
+  },
+  {
+    "revision": "9bb211ba8b31e5c2d6ba",
+    "url": "js/app.443261b3.js"
+  },
+  {
+    "revision": "f6d0072c102fd3326a7f",
+    "url": "js/chunk-2dde0b94.11949619.js"
+  },
+  {
+    "revision": "bd61a456be46f8773548",
+    "url": "js/chunk-vendors.6585eb59.js"
+  },
+  {
+    "revision": "ff52cf4c77656aa0d750c944bb9bb4e6",
+    "url": "img/vechainstats-logo.ff52cf4c.png"
+  },
+  {
+    "revision": "db35660f3e5f16bf01cf05cb531c60fa",
+    "url": "index.html"
+  },
+  {
+    "revision": "94950714edb823ff37d085ed03466498",
+    "url": "404.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
